6eb2ff616ca9a8ef51d1d10f967dc6c55435108f
Prasad <sj6282@snaponglobal.com>
2018-08-19 21:10:35 +0530

    commit after first review

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 // Registering the service worker
+// 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('sw.js').then(function(registration) {
